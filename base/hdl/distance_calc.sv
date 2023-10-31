60c0d30c16aead44758d70b9398acd5255e17904
Sanjay Seshan <sanjay.seshan@outlook.com>
2023-10-31 19:02:32 -0400

    update dist

--- base/hdl/distance_calc.sv
+++ base/hdl/distance_calc.sv
@@ -4,10 +4,10 @@
 module distance #(parameter DIM = 2)(
   input wire clk_in,
   input wire rst_in,
-  input wire data_valid_in,
-  input wire [31:0] vertex_in;
-  input wire [31:0] query_in;
-  output logic [15:0] distance_out,
+  input wire data_valid_in [DIM-1:0],
+  input wire [31:0] vertex_pos_in [DIM-1:0];
+  input wire [31:0] query_pos_in [DIM-1:0];
+  output logic [15:0] distance_sq_out,
   output logic valid_data_out
   );
 
@@ -16,6 +16,26 @@ module distance #(parameter DIM = 2)(
   logic point_mul_tvalid, query_mul_tvalid;
 
 
+  output logic valid_subs_out [DIM-1:0];
+
+
+  for (i=0; i < DIM; i=i+1) begin
+
+    adder sub(
+      .aclk(clk_in), 
+
+      .s_axis_a_tvalid(data_valid_in[i]), 
+      .s_axis_a_tdata(query_pos_in[i]), 
+
+      .s_axis_b_tvalid(1), 
+      .s_axis_b_tdata({1,query_pos_in[i][30:23],~query_pos_in[i][30:23]}), 
+
+      .m_axis_result_tvalid(valid_data_out), 
+      .m_axis_result_tready(1'b1), 
+      .m_axis_result_tdata(distance_out)
+      );
+
+  end
 
   multiplier mult1(
     .aclk(clk_in), 
