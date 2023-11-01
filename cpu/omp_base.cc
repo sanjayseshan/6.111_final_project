96222586ae9ee2b45ccceb47afa0d71ac45bfc94
lablab987 <lasyabalachandran@gmail.com>
2023-11-01 12:14:05 -0400

    supposedly working cpu implementation

--- cpu/omp_base.cc
+++ cpu/omp_base.cc
@@ -8,6 +8,7 @@
 #include <queue>
 #include <iostream>
 #include <algorithm>
+#include <bits/stdc++.h>
 // using namespace std;
 
 void ANN(Graph &g, std::vector<Embedding> points, Embedding query) {
@@ -28,102 +29,128 @@ void ANN(Graph &g, std::vector<Embedding> points, Embedding query) {
     // int visited[g.V] = 0;
 
     // fixed length L
-    int L = 20;
+    int L = 50;
 
     // how many elements to return 
-    int k = 10;
+    int k = 30;
 
     // create visited list
     int visited [points.size()] = { 0 }; 
-    std::priority_queue<tuple<int,int,vidType>> checked; 
-    // std::priority_queue<tuple<int,int,vidType>> checked;
 
-    int dist;
-    // std::priority_queue<tuple<int,std::vector<float>>> S;
-    std::priority_queue<tuple<int,int,vidType>> S;
-    // std::priority_queue<tuple<vidType,std::vector<float>>> output;
-    // std::list<tuple<vidType,std::vector<float>>> output;
+    // priority queues
+    std::priority_queue<tuple<float,int>, vector<tuple<float, int>>, greater<tuple<float, int>>> S;
+    std::priority_queue<tuple<float,int>> checked; 
+
     std::list<int> output;
 
     int index = 0;
-    int P = g.V(index);
     Embedding P_loc = points[index];
 
+    // for debugging
     // std::cout << "P loc size: " << P_loc.size();
 
-    for(Embedding i:points) {
-      std::cout << "here";
-      for (float j:i) {
-        std::cout << "P: " << j;
-      }
-    }
 
-    dist = 0;
+    // distance calculation
+    float dist = 0.0;
     for(int i=0; i< P_loc.size(); i++){
-      dist += pow(P_loc[i]-query[i], 2);
-      std::cout << "P location: " << P_loc[i] << "\nQuery location: " << query[i];
+      dist = dist + pow((P_loc[i]-query[i]), 2);
+
+      // for debugging
+      // std::cout << "\nP location: " << P_loc[i] << ", Query location: " << query[i] << " dist: " << dist;
     }
     dist = pow(dist, 0.5);
+    //std::cout << "dist: " << dist << "\n";
 
-    std::cout << "dist: " << dist;
-
-    
-    // // pq.push(make_tuple(starting_point);
-    S.push(make_tuple(dist, index, P));
+    // add starting point to S
+    S.push(make_tuple(dist, index));
+    visited[index] = 1;
 
 
     while (!S.empty()) {
       // i = index of first unchecked vertex
-      index = get<1>(S.top());
-      
-      // get vertex from pq
-      vidType v = get<2>(S.top());
+      vidType v = get<1>(S.top());
+
+      // for debugging
+      // std::cout << "\nindex: " << v;
 
       // marked v_i as checked
-      checked.push(S.top());
+      if (checked.size() < L || get<0>(S.top()) < get<0>(checked.top())) {
+        checked.push(S.top());
+
+        if (checked.size()>L) {
+          checked.pop();
+        }
+      }
+      else {
+        if(checked.size()==L) {
+          break;
+        }
+      }
       S.pop();
+
+      // for debugging
+      // std::cout << "\nS size: " << S.size();
+      // std::cout << "\nchecked size: " << checked.size();
 
       // iterate through neighbors
       for(vidType u:g.N(v)) {
-        if (visited[v] == 0) {
-          visited[v] = 1;
-        
-        // if(std::find(visited.begin(), visited.end(), u) != visited.end()) {
-        //   visited.push_back(u);
+        if (visited[u] == 0) {
+          visited[u] = 1;
 
-          std::vector<float> point = points[v];
-          dist = 0;
+          // for debugging
+          // std::cout << "\nu: " << u;
+
+          // find distance
+          std::vector<float> point = points[u];
+          dist = 0.0;
           for(int i=0; i< point.size(); i++){
             dist += pow(point[i]-query[i], 2);
           }
           dist = pow(dist, 0.5);
+          // std::cout << "\ndist: " << dist;
 
-          if (dist < get<0>(S.top()) && S.size()>=L) {
-            S.push(make_tuple(dist, v, u));
+          // add to S 
+          // float max_dist = get<0>(S.top());
+          // if (dist < max_dist || S.size() < L) {
+            S.push(make_tuple(dist, u));
+          // }
+
+          // resize based on L
+          std::vector<tuple<float, int>> temp;
+          if (S.size() > L) {
+            while (S.size() > 1) {
+              temp.push_back(S.top());
+              S.pop();
+            }
+            S.pop();
+            for (tuple<float, int> element: temp){
+              S.push(element);
+            }
+            temp.clear();
           }
         }
       }
     }
 
-    // }
-
     while (checked.size() > k) {
       checked.pop();
     }
-    for (int i=0; i<k; i++) {
-      // output.push_front(make_tuple(get<2>(checked.top()), points[get<1>(checked.top())]));
+    
+    int checked_length = checked.size();
+    for (int i=0; (i<k && i<checked_length); i++) {
       int element = get<1>(checked.top());
+      float dist = get<0>(checked.top());
       output.push_front(element);
       checked.pop();
 
-      std::cout << element << '\n';
+      std::cout << "\nelement: " << element << ", dist: " << dist;
     }
 
     // std::cout << "output = " << output;
-  
+
 
   t.Stop();
-  std::cout << "runtime [omp_base] = " << t.Seconds() << " sec\n";
+  std::cout << "\nruntime [omp_base] = " << t.Seconds() << " sec\n";
   return;
 }
 
