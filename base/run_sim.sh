b3281aca0f20a6289b51b1f38404ea0119427234
Sanjay Seshan <sanjay.seshan@outlook.com>
2023-12-08 14:26:55 -0500

    making checked sequential wip

--- base/run_sim.sh
+++ base/run_sim.sh
@@ -10,3 +10,12 @@ xelab -svlog sim/bfis_tb.sv --debug wave
 rm dump.vcd
 # xsim checked_tb -t xsim_run.tcl
 xsim bfis_tb -t xsim_run.tcl
+
+
+
+# xvlog --sv ./hdl/*
+# # xelab -svlog sim/bfis_tb.sv --debug wave
+# xelab -svlog sim/checked_tb.sv --debug wave
+# rm dump.vcd
+# xsim checked_tb -t xsim_run.tcl
+# # xsim bfis_tb -t xsim_run.tcl
